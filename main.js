ee4fa2110c7a72a866953a22f1db4d880f557b3f
wanghao <1275851274@qq.com>
2022-10-25 18:52:44 +0800

    初始项目

---
+++ main.js
@@ -0,0 +1,38 @@
+
+// #ifndef VUE3
+import Vue from 'vue'
+import App from './App'
+import { $http } from '@escook/request-miniprogram'
+import store from 'store/index.js'
+uni.$http = $http
+$http.baseUrl = 'https://www.showdoc.com.cn/p/'
+
+$http.beforeRequest = function (options) {  
+    uni.showLoading({
+    	title: '数据加载中...',
+	})
+}
+$http.afterRequest = function () {  
+    uni.hideLoading()
+}
+uni.$http = $http
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App,
+	store
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
---
+++ pages/home/home.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<view class="ipt">
+			<text>天天童装</text>
+			<input type="text" placeholder="输入关键词搜索">
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.ipt {
+		width: 100%;
+		position: fixed;
+		top: 0;
+		padding-bottom: 10px;
+		display: flex;
+		border-bottom: 1px solid #aaa;
+		text{
+			font-weight: bold;
+		}
+		input{
+			border: 1px solid #aaa;
+			border-radius: 10px;
+			margin-left: 30px;
+			padding-left: 50px;
+		}
+	}
+</style>
